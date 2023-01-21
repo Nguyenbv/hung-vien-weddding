927908a66adea725e93ffe179a3c40042c879b32
nguyenloki258 <nguyenbv1994@gmail.com>
2023-01-21 23:32:23 +0700

    feat: update info

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Schedule from "./components/schedule";
 import Contact from "./components/contact";
 import Invitation from "./components/invitation";
 import Congrats from './components/congrats';
+import { Helmet } from 'react-helmet'
 
 function App() {
   const fullPage = useRef();
@@ -113,7 +114,17 @@ function App() {
   const location = urlParams.get("location") || "nha-trai";
 
   return (
+
     <div className="App" ref={fullPage} onScroll={onScroll}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{location === 'nha-trai' ? "Lễ thành hôn | 30/01/2023" : "Lễ vu quy| 30/01/2023"}</title>
+        <meta property="og:title" content={location === 'nha-trai' ? "Lễ thành hôn | 30/01/2023" : "Lễ vu quy| 30/01/2023"} />
+        <meta property="og:site_name" content={location === 'nha-trai' ? "Lễ thành hôn | 30/01/2023" : "Lễ vu quy| 30/01/2023"} />
+        <meta property="og:site_name" content={location === 'nha-trai' ? "Lễ thành hôn | 30/01/2023" : "Lễ vu quy| 30/01/2023"} />
+        <meta name="twitter:site" content={location === 'nha-trai' ? "Lễ thành hôn | 30/01/2023" : "Lễ vu quy| 30/01/2023"} />
+        <meta name="twitter:title" content={location === 'nha-trai' ? "Lễ thành hôn | 30/01/2023" : "Lễ vu quy| 30/01/2023"} />
+      </Helmet>
       <a
         href={getInvitation()}
         className="action-btn"
